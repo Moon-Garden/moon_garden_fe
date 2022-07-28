089ec7e7e1075e6edbcc7fba68352a710d6c6855
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-28 19:00:25 -0400

    Consume find_or_create_user endpoint, remove stubs

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,6 +1,6 @@
 class BaseService 
     def self.connection
-        Faraday.new(url: 'https://moon-garden-be.herokuapp.com/' )
+        Faraday.new(url: 'https://moon-garden-be.herokuapp.com' )
     end
 
     def self.get_json(response)
--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'user service' do
     it 'creates and returns a new user' do 
-        json_response = {
-            "data": {
-              "id": "1",
-              "type": "user",
-              "attributes": {
-                "name": "Jennifer",
-                "email": 'jenniferlhalloran@gmail.com',
-                "image": "https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-cﬁ",
-              }
-            }
-        }.to_json
+        # json_response = {
+        #     "data": {
+        #       "id": "1",
+        #       "type": "user",
+        #       "attributes": {
+        #         "name": "Jennifer",
+        #         "email": 'jenniferlhalloran@gmail.com',
+        #         "image": "https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-cﬁ",
+        #       }
+        #     }
+        # }.to_json
 
         user_hash = 
         {"name"=>"Jennifer Halloran",
@@ -23,20 +23,19 @@ RSpec.describe 'user service' do
             "last_name"=>"Halloran",
             "image"=>"https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-c"}
 
-            stub_request(:post, "https://moon-garden-be.herokuapp.com/api/v1/users").
-            with(
-              body: "{\"name\":\"Jennifer Halloran\",\"email\":\"jenniferlhalloran@gmail.com\",\"unverified_email\":\"jenniferlhalloran@gmail.com\",\"email_verified\":true,\"first_name\":\"Jennifer\",\"last_name\":\"Halloran\",\"image\":\"https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-c\"}",
-              headers: {
-             'Accept'=>'*/*',
-             'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-             'Content-Type'=>'application/json',
-             'User-Agent'=>'Faraday v2.4.0'
-              }).
-            to_return(status: 200, body: json_response, headers: {})
+        #     stub_request(:post, "https://moon-garden-be.herokuapp.com/api/v1/users").
+        #     with(
+        #       body: "{\"name\":\"Jennifer Halloran\",\"email\":\"jenniferlhalloran@gmail.com\",\"unverified_email\":\"jenniferlhalloran@gmail.com\",\"email_verified\":true,\"first_name\":\"Jennifer\",\"last_name\":\"Halloran\",\"image\":\"https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-c\"}",
+        #       headers: {
+        #      'Accept'=>'*/*',
+        #      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        #      'Content-Type'=>'application/json',
+        #      'User-Agent'=>'Faraday v2.4.0'
+        #       }).
+        #     to_return(status: 200, body: json_response, headers: {})
 
             
             response = UserService.create_user(user_hash)
-            
             expect(response).to be_a Hash
             expect(response[:data]).to be_a Hash
             expect(response[:data]).to have_key :id
