87bec853fc8020de47d075d45270e07138f804a8
Jim Riddle <jimriddle0@gmail.com>
2022-07-29 11:04:28 -0500

    place holder test for garden names

---
+++ spec/features/dashboard/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Dashboard Show Page' do
+    context "a user is logged in" do
+        it "gives me the gardens of the user", :vcr do
+            Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+
+            visit '/'
+            click_on 'Login'
+
+            expect(current_path).to eq('/dashboard')
+
+            expect(page).to have_content("Gardens:")
+
+            # save_and_open_page
+        end
+    end
+end
