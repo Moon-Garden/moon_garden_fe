fc4f8ba854311d9ebd8f7a9889322862bc2c039b
Jim Riddle <jimriddle0@gmail.com>
2022-07-29 11:03:46 -0500

    added notes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     if auth_hash['credentials']['token'].present?
       user = UserFacade.find_or_create_user(auth_hash[:info])
-      session[:id] = user.id
+      session[:id] = user.id #this is a string
       redirect_to '/dashboard'
      end
   end
