7e6b4ef53555070ebb470e630013a4b6d01bc28c
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-30 07:42:28 -0600

    Passing test for moon garden about layout

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Welcome Index Page' do
-    context "a user is not logged in" do
-        it "allows the user to log in through Google", :vcr do
-            Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  context "a user is not logged in" do
+    it "allows the user to log in through Google", :vcr do
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
 
-            visit '/'
-            click_on 'Login'
+      visit '/'
+      click_on 'Login'
 
-            expect(current_path).to eq('/dashboard')
-        end
+      expect(current_path).to eq('/dashboard')
     end
+
+    it 'has an about section with the idea behind the app' do 
+      visit '/'
+      
+      within '.main-bar' do 
+        expect(page).to have_content('About Moon Garden')
+      end 
+      within '.side-bar-left' do 
+        expect(page).to have_content('Moon Garden')
+      end 
+    end
+  end
 end
