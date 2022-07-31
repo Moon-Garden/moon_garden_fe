dfeb33fbaa49e4f52bb2b3cef619740b2e1567cc
Stephen Wilkens <stephenwilkens@gmail.com>
2022-07-31 15:06:07 -0600

    add feature tests for usres dashbaord

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -2,13 +2,36 @@ require 'rails_helper'
 
 RSpec.describe 'Dashboard Show Page' do
   context 'a user is logged in' do
-    it 'gives me the gardens of the user', :vcr do
+    before :each do
       Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
-
       visit '/'
       click_on 'Login'
+    end
 
-      expect(current_path).to eq('/dashboard')
+# !!! THE EXPECT STATEMENTS ARE MOCKS AND STILL NEED TO BE FILLED WITH CORRECT INFO !!!
+    xit 'diplays the title of the app' do
+      expect(page).to have_content("Moon Garden")
+    end
+
+    xit 'has a section title "(Users name)s Gardens" with all their gardens listed' do
+      expect(page).to have_content("User's Gardens")
+      expect(page).to have_content("Summer Garden")
+      expect(page).to have_content("Winter Garden")
+      expect(page).to_not have_content("Stupid dumbo idiot Garden")
+    end
+
+    xit 'each garden listed is a link to that gardens show page' do
+      click_link("Summer Garden")
+      expect(current_path).to eq("/users/#{user.id}/gardens/#{summer_garden.id}")
+    end
+
+    xit 'has a button to create a new garden' do
+      click_button("New Garden")
+      expect(current_path).to eq("/users")
+    end
+
+    xit 'displays the lunar recommendations' do
+      #not sure how to test for this
     end
   end
 end
