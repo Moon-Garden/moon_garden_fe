674aeb971782ee3a30a17d95faca1b6bf3126f75
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-07-31 19:07:29 -0400

    Update header

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
   get '/auth/google_oauth2/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   get '/dashboard', to: 'dashboard#show'
 end
