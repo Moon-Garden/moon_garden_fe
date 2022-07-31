d374221eaa3ee5dc69d92a0d0226c1d7d27db34c
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-07-31 17:29:06 -0600

    Circle CI fix

--- spec/features/dashboard/show_spec.rb
+++ spec/features/dashboard/show_spec.rb
@@ -25,22 +25,5 @@ RSpec.describe 'Dashboard Show Page' do
     it "displays each user garden with garden info and a link to the garden's show page" do
       
     end
-
-    it "has a link to add a new garden", :vcr do
-      visit '/'
-      click_on 'Login'
-
-      click_on "Add New Garden"
-      expect(current_path).to eq('/gardens/new')
-      
-      # fill_in 'Garden Name', with: "Asparagus Garden"
-      # select 'East', from: "Cardinal Direction"
-      # fill_in 'Notes', with: "Only asparagus 4 lyfe"
-
-      # click_on "Submit"
-
-      # expect(current_path).to eq('/dashboard')
-      # expect(page).to have_content("Asparagus Garden")
-    end
   end
 end
