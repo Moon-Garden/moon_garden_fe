7c8e4527efe21b257fcf7bfb8d1d5f67543e53bf
Jim Riddle <jimriddle0@gmail.com>
2022-08-01 17:11:02 -0500

    create method flushed out

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -6,6 +6,15 @@ class PlantsController < ApplicationController
   end
 
   def create
+    data = {
+      "user_id": session[:user_id],
+      "garden_id": params[:id],
+      "name": params[:plant_name],
+      "plant_id": params[:plant_api_id]
+    }
 
+    # binding.pry
+    PlantTrackingFacade.create_plant(data)
+    redirect_to "/gardens/#{params[:id]}"
   end
 end
