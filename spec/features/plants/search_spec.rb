a3cd7d24b02c45bb1bd00ae4cf505811c743c842
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-08-01 13:57:08 -0600

    Add fixture file for search spec

--- spec/features/plants/search_spec.rb
+++ spec/features/plants/search_spec.rb
@@ -9,11 +9,13 @@ RSpec.describe 'plant search page' do
       @gardens = JSON.parse(File.read('spec/fixtures/gardens.json'), symbolize_names: true)
       @hotdog_garden = JSON.parse(File.read('spec/fixtures/hotdog_garden.json'), symbolize_names: true)
       @moon = JSON.parse(File.read('spec/fixtures/moon.json'),symbolize_names: true)
+      @results = JSON.parse(File.read('spec/fixtures/tomato_search.json'), symbolize_names: true)
 
       allow(UserService).to receive(:find_or_create_user).and_return(@user)
       allow(GardenService).to receive(:get_gardens).and_return(@gardens)
       allow(GardenService).to receive(:get_garden_info).and_return(@hotdog_garden)
       allow(MoonService).to receive(:get_moon_data).and_return(@moon)
+      allow(PlantService).to receive(:search_plant_data).and_return(@results)
 
       visit '/'
       click_on 'Login'
@@ -25,12 +27,12 @@ RSpec.describe 'plant search page' do
       click_on 'Find Plants to add to Garden'
 
       expect(current_path).to eq("/gardens/339/plants")
-      expect(page).to have_content('Name: corn')
+      expect(page).to have_content('Name: Tiny Tim Tomato')
       expect(page).to have_content('Sun Requirements: Full Sun')
-      expect(page).to have_content('Sowing Method: Direct seed outdoors')
-      expect(page).to have_content('Row Spacing: 76')
-      expect(page).to have_content('Description: Corn is a large grain plant, or tall grass, first domesticated about 10,000 years ago by indigenous peoples in Southern Mexico. The leafy stalk produces ears after pollination. Depending on the variety, the corn can be eaten fresh, or dried and ground into cornmeal.')
-      expect(page).to_not have_content('basil')
+      expect(page).to have_content('Sowing Method: Direct seed indoors, transplant seedlings outside after hardening off')
+      expect(page).to have_content('Row Spacing: 45')
+      expect(page).to have_content('Description: The tomato is the fruit of the tomato plant, a member of the Nightshade family (Solanaceae). The fruit grows on a small compact bush.')
+      expect(page).to_not have_content('corn')
     end 
   end
 end
