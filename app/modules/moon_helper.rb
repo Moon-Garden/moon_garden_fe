abfaa014f9a266b28803b767359f1f31d8414cbf
Bryce Wein <bryce.wein@gmail.com>
2022-08-01 13:14:16 -0400

    Refactor: change moonage

--- app/modules/moon_helper.rb
+++ app/modules/moon_helper.rb
@@ -2,11 +2,11 @@ module MoonHelper
   def self.rec(phase)
     moon_phase = phase.downcase
 
-    if moon_phase.include?('new')
+    if moon_phase.include?('dark')
       'Plant crops which produce seeds outside the fruit (grains, spinach, cauliflower, cabbage, broccoli, celery and lettuce).'
     elsif moon_phase.include?('waxing')
       'Plant crops with seeds inside the fruit (beans, peppers, tomatoes, squash and melons).'
-    elsif moon_phase.include?('dark')
+    elsif moon_phase.include?('full')
       "Root vegetables, such as carrots, beets, onions and potatoes, will flourish. It's also a good time to transplant seedlings or to prune."
     elsif moon_phase.include?('waning')
       'Avoid planting and focus on fertilizing the soil. This is the best time to mow grass, harvest, transplant and prune.'
