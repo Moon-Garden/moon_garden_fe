247d922d37575696995286601f00447502cd89f0
Bryce Wein <bryce.wein@gmail.com>
2022-08-01 12:39:35 -0400

    Feat: create moon recommendation card

---
+++ app/modules/moon_helper.rb
@@ -0,0 +1,17 @@
+module MoonHelper
+  def self.rec(phase)
+    moon_phase = phase.downcase
+
+    if moon_phase.include?('new')
+      'Plant crops which produce seeds outside the fruit (grains, spinach, cauliflower, cabbage, broccoli, celery and lettuce).'
+    elsif moon_phase.include?('waxing')
+      'Plant crops with seeds inside the fruit (beans, peppers, tomatoes, squash and melons).'
+    elsif moon_phase.include?('dark')
+      "Root vegetables, such as carrots, beets, onions and potatoes, will flourish. It's also a good time to transplant seedlings or to prune."
+    elsif moon_phase.include?('waning')
+      'Avoid planting and focus on fertilizing the soil. This is the best time to mow grass, harvest, transplant and prune.'
+    else
+      "Interesting, this is a moon we don't have anything for! We will work on that!"
+    end
+  end
+end
