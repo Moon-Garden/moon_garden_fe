414e980887e150db8fd139c71d126be5b56751af
Bryce Wein <bryce.wein@gmail.com>
2022-08-01 13:39:50 -0400

    Test: refactor tests

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
     if session[:user_id]
-      @gardens = GardenFacade.get_garden_info(session[:user_id])[0..5]
+      @gardens = GardenFacade.get_gardens(session[:user_id])[0..5]
     else
       redirect_to '/'
     end
