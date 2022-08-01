9c661f279316e944cbef92ef0fbfe41da0bbec1a
Andrew King <dev.andrewking@gmail.com>
2022-08-01 15:24:58 -0500

    change name of land to geocoder

--- app/services/geocoder_service.rb
+++ app/services/geocoder_service.rb
@@ -1,5 +1,5 @@
 class GeocoderService
-  def self.get_latlng(location)
+  def self.get_coordinates(location)
     response = BaseService.geocoder_connection
     .get("/geocoding/v1/address/?key=#{ENV['MAPQUEST_KEY']}&location=#{location}", content_type: 'application/json')
     BaseService.get_json(response)
