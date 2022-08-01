839b9c92824e586a3a995577ef8dab7256ea72fe
Bryce Wein <bryce.wein@gmail.com>
2022-07-31 20:55:35 -0400

    Refactor: change dates

--- spec/poros/plant_tracking_spec.rb
+++ spec/poros/plant_tracking_spec.rb
@@ -1,25 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe PlantTracking do 
-  it 'exists and has attributes' do 
-    plant_params = 
-    {
-      "name": "Carrot", 
-      "plant_id": "sae2340987dage",
-      "moon_phase": "waxing crescent",
-      "date_planted": Date.today,
-      "date_matured": Date.today,
-      "bounty_amount": 23,
-      "pruning_behaviors": "No pruning, only thinning",
-      "notes": "Magic carrots matured on the same day they were planted!" 
-     }
+RSpec.describe PlantTracking do
+  it 'exists and has attributes' do
+    plant_params =
+      {
+        "name": 'Carrot',
+        "plant_id": 'sae2340987dage',
+        "moon_phase": 'waxing crescent',
+        "date_planted": Date.today,
+        "date_matured": Date.today,
+        "bounty_amount": 23,
+        "pruning_behaviors": 'No pruning, only thinning',
+        "notes": 'Magic carrots matured on the same day they were planted!'
+      }
 
     plant = PlantTracking.new(plant_params)
 
     expect(plant).to be_an_instance_of(PlantTracking)
     expect(plant.name).to eq('Carrot')
-    expect(plant.date_matured).to eq('2022-07-31')
-    expect(plant.date_planted).to eq('2022-07-31')
+    expect(plant.date_matured).to eq(Date.today)
+    expect(plant.date_planted).to eq(Date.today)
     expect(plant.moon_phase).to eq('waxing crescent')
     expect(plant.bounty_amount).to eq(23)
     expect(plant.pruning_behaviors).to eq('No pruning, only thinning')
