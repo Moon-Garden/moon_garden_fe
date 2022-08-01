d605f065ebcc123ad68c0362cc4c50c4c95a6506
Bryce Wein <bryce.wein@gmail.com>
2022-08-01 11:00:18 -0400

    Refactor: add illumination to moon model

--- app/poros/moon.rb
+++ app/poros/moon.rb
@@ -1,9 +1,10 @@
 class Moon
-  attr_reader :phase, :name
+  attr_reader :phase, :name, :illumination
 
   def initialize(data)
     @phase = data[:Phase]
     @name = format_moon_name(data[:Moon])
+    @illumination = (data[:Illumination] * 100).to_i
   end
 
   def format_moon_name(array)
