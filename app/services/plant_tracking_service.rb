3c05660afa9f4c0a66d853d6c90bff14962a0e4d
Jim Riddle <jimriddle0@gmail.com>
2022-08-01 10:21:10 -0500

    included two methods on the plant tracking service

--- app/services/plant_tracking_service.rb
+++ app/services/plant_tracking_service.rb
@@ -2,7 +2,7 @@ class PlantTrackingService
   def self.create_plant(attributes)
     # binding.pry
     response = BaseService.connection.post(
-      "/api/v1/users/#{attributes[:user_id]}/gardens/#{attributes[:garden_id]}/plants", 
+      "/api/v1/users/#{attributes[:user_id]}/gardens/#{attributes[:garden_id]}/plants",
       _json: attributes.to_json,
       content_type: 'application/json'
     )
@@ -13,4 +13,18 @@ class PlantTrackingService
     response = BaseService.connection.get("/api/v1/users/#{user_id}/gardens/#{garden_id}/plants")
     BaseService.get_json(response)
   end
+
+  def self.destroy_plant(user_id, garden_id, plant_id)
+    response = BaseService.connection.delete("/api/v1/users/#{user_id}/gardens/#{garden_id}/plants/#{plant_id}")
+  end
+
+  def self.edit_plant(user_id, garden_id, plant_id, attributes)
+    response = BaseService.connection.patch(
+      "/api/v1/users/#{user_id}/gardens/#{garden_id}/plants/#{plant_id}",
+      _json: attributes.to_json,
+      content_type: 'application/json'
+    )
+    BaseService.get_json(response)
+  end
+  
 end
