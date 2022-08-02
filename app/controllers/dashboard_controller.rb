5844a0c1e83f2241b5241f3010331e6e9109b691
Andrew King <dev.andrewking@gmail.com>
2022-08-02 12:18:45 -0500

    add default param

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def show
-    # binding.pry
     @gardens = GardenFacade.get_gardens(session[:user_id])[0..5]
+    if !params[:location]
+      params[:location] = 'Washington, DC'
+    end
+      @ll = GeocoderFacade.get_coordinates(params[:location])
   end
 end
