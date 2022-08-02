8ace22b8ac99333c63a356f96340d0a52f0d2a87
Bryce Wein <bryce.wein@gmail.com>
2022-08-01 23:57:59 -0400

    Test: make plants search spec test pass

--- spec/features/plants/search_spec.rb
+++ spec/features/plants/search_spec.rb
@@ -2,32 +2,35 @@ require 'rails_helper'
 
 RSpec.describe 'plant search page' do
   context 'happy path' do
-    # before do
-    #   Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
-    #
-    #   @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
-    #   @gardens = JSON.parse(File.read('spec/fixtures/gardens.json'), symbolize_names: true)
-    #   @hotdog_garden = JSON.parse(File.read('spec/fixtures/hotdog_garden.json'), symbolize_names: true)
-    #   @moon = JSON.parse(File.read('spec/fixtures/moon.json'),symbolize_names: true)
-    #   @results = JSON.parse(File.read('spec/fixtures/tomato_search.json'), symbolize_names: true)
-    #
-    #   allow(UserService).to receive(:find_or_create_user).and_return(@user)
-    #   allow(GardenService).to receive(:get_gardens).and_return(@gardens)
-    #   allow(GardenService).to receive(:get_garden_info).and_return(@hotdog_garden)
-    #   allow(MoonService).to receive(:get_moon_data).and_return(@moon)
-    #   allow(PlantService).to receive(:search_plant_data).and_return(@results)
-    #
-    #   visit '/'
-    #   click_on 'Login'
-    # end
+    before do
+      Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
 
-    xit 'returns the top 5 results related to a search', :vcr do
+      @user = JSON.parse(File.read('spec/fixtures/user.json'), symbolize_names: true)
+      @gardens = JSON.parse(File.read('spec/fixtures/gardens.json'), symbolize_names: true)
+      @hotdog_garden = JSON.parse(File.read('spec/fixtures/hotdog_garden.json'), symbolize_names: true)
+      @moon = JSON.parse(File.read('spec/fixtures/moon.json'), symbolize_names: true)
+      @results = JSON.parse(File.read('spec/fixtures/tomato_search.json'), symbolize_names: true)
+      @plants = JSON.parse(File.read('spec/fixtures/plants.json'), symbolize_names: true)
+
+      allow(UserService).to receive(:find_or_create_user).and_return(@user)
+      allow(GardenService).to receive(:get_gardens).and_return(@gardens)
+      allow(GardenService).to receive(:get_garden_info).and_return(@hotdog_garden)
+      allow(MoonService).to receive(:get_moon_data).and_return(@moon)
+      allow(PlantService).to receive(:search_plant_data).and_return(@results)
+      allow(PlantTrackingService).to receive(:get_plants).and_return(@plants)
+
+      visit '/'
+      click_on 'Login'
+    end
+
+    it 'returns the top 5 results related to a search', :vcr do
       visit '/gardens/339'
 
       fill_in :search, with: 'tomato'
+
       click_on 'Find Plants to add to Garden'
 
-      expect(current_path).to eq("/gardens/339/plants")
+      expect(current_path).to eq('/gardens/339/plants')
       expect(page).to have_content('Name: Tiny Tim Tomato')
       expect(page).to have_content('Sun Requirements: Full Sun')
       expect(page).to have_content('Sowing Method: Direct seed indoors, transplant seedlings outside after hardening off')
@@ -46,9 +49,9 @@ RSpec.describe 'plant search page' do
           'last_name' => 'Dog',
           'image' => 'https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-c' }
 
-        user = UserFacade.find_or_create_user(user_hash)
+      user = UserFacade.find_or_create_user(user_hash)
 
-        garden_hash =
+      garden_hash =
         {
           "user_id": "#{user.id}",
           "name": 'Summer Garden',
@@ -67,14 +70,12 @@ RSpec.describe 'plant search page' do
       click_on 'Find Plants to add to Garden'
 
       expect(page).to have_button 'Add Plant to Garden'
-      within "#Tiny" do
+      within '#Tiny' do
         click_button 'Add Plant to Garden'
       end
 
       expect(current_path).to eq("/gardens/#{garden.id}")
-      expect(page).to have_content("Tiny Tim Tomato")
-
+      expect(page).to have_content('Tiny Tim Tomato')
     end
-    
   end
 end
