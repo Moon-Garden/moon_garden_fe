06a6548616d24a49ba7d3136ce5652d229cf27b0
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-08-03 19:55:03 -0400

    Update garden facade tests to destroy each created garden

--- spec/services/garden_service_spec.rb
+++ spec/services/garden_service_spec.rb
@@ -34,6 +34,8 @@ RSpec.describe 'garden be service' do
       expect(@response[:data][:attributes]).to have_key :name
       expect(@response[:data][:attributes]).to have_key :cardinal_direction
       expect(@response[:data][:attributes]).to have_key :notes
+      
+      GardenFacade.destroy_garden(@user_response[:data][:id], @response[:data][:id])
     end
   end
 
@@ -48,6 +50,8 @@ RSpec.describe 'garden be service' do
     expect(response[:data].first[:attributes]).to have_key(:name)
     expect(response[:data].first[:attributes]).to have_key(:cardinal_direction)
     expect(response[:data].first[:attributes]).to have_key(:notes)
+
+    GardenFacade.destroy_garden(@user_response[:data][:id], @response[:data][:id])
   end
 
   it 'gets a single gardens information', :vcr do
@@ -61,36 +65,20 @@ RSpec.describe 'garden be service' do
     expect(response[:data][:attributes]).to have_key(:cardinal_direction)
     expect(response[:data][:attributes]).to have_key(:name)
     expect(response[:data][:attributes]).to have_key(:notes)
+
+    GardenFacade.destroy_garden(@user_response[:data][:id], @response[:data][:id])
   end
 
   context 'destroy_garden' do
     it 'destroys a users garden', :vcr do
-      user_hash =
-        { 'name' => 'Jennifer Halloran',
-          'email' => 'frankfurters@weinermobile.com',
-          'unverified_email' => 'frankfurters@weinermobile.com',
-          'email_verified' => true,
-          'first_name' => 'Jennifer',
-          'last_name' => 'Halloran',
-          'image' => 'https://lh3.googleusercontent.com/a-/AFdZucr_zffBdhJaydFkdXeeHkhe2BzmVNKGIE-Ozwvh=s96-c' }
 
-      user = UserService.find_or_create_user(user_hash)
+      garden = GardenFacade.create_garden(@garden_hash)
 
-      garden_hash =
-        {
-          "user_id": "#{user[:data][:id]}",
-          "name": 'Summer Garden',
-          "notes": "it's too damn hot",
-          "cardinal_direction": 1
-        }
+      user_gardens_length1 = GardenFacade.get_gardens(@user_response[:data][:id]).length
 
-      garden = GardenFacade.create_garden(garden_hash)
+      GardenService.destroy_garden(@user_response[:data][:id], garden.id)
 
-      user_gardens_length1 = GardenFacade.get_gardens(user[:data][:id]).length
-
-      GardenService.destroy_garden(user[:data][:id], garden.id)
-
-      user_gardens_length2 = GardenFacade.get_gardens(user[:data][:id]).length
+      user_gardens_length2 = GardenFacade.get_gardens(@user_response[:data][:id]).length
 
       expect(user_gardens_length2).to_not eq(user_gardens_length1)
     end
