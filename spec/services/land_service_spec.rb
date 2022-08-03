62009829059ed309941c5970943f080eaf8c7229
Andrew King <dev.andrewking@gmail.com>
2022-08-03 11:20:28 -0500

    add second test to land service spec

--- spec/services/land_service_spec.rb
+++ spec/services/land_service_spec.rb
@@ -12,5 +12,18 @@ RSpec.describe 'land service' do
       expect(response.first[:properties]).to be_a Hash
       expect(response.first[:properties][:Name]).to eq('Conoy')
     end
+
+    it 'returns multiple names', :vcr do
+      lat = '42.588079'
+      lng = '-87.822877'
+      
+      response = LandService.get_land_data(lat, lng)
+      expect(response).to be_an Array
+      expect(response.first).to be_a Hash
+      expect(response.first[:properties]).to be_a Hash
+      expect(response.first[:properties][:Name]).to eq('Dakȟóta (Western Dakota)')
+      expect(response[1][:properties][:Name]).to eq('Dakhóta (Eastern Dakota)')
+      expect(response[2][:properties][:Name]).to eq('Hoocąk (Ho-Chunk)')
+    end
   end
 end
