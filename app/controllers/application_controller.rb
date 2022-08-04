688d9902864223d2b15317fddea46ff46fdbee29
Jim Riddle <jimriddle0@gmail.com>
2022-08-04 17:18:46 -0500

    modified controllers to include contributor page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :set_lunar_rec
   helper_method :get_weather, :ip_address
 
-  def authorize_user 
+  def authorize_user
     unless session[:user_id]
       flash[:alert] = 'Oopsy daisy! Please log in below to view that page.'
       redirect_to '/'
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
     else
       Net::HTTP.get(URI.parse('http://checkip.amazonaws.com/')).squish
     end
-  end 
+  end
 
   def get_weather
     location = IpFacade.get_ip_location(ip_address)
@@ -28,4 +28,8 @@ class ApplicationController < ActionController::Base
     long = location.coordinates.split(",").last
     WeatherFacade.get_weather(lat, long)
   end
+
+  def contributors
+  end
+  
 end
