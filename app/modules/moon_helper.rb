183b52e5c85f004680c6b3fe22165bae2d71979e
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-18 18:53:04 -0600

    actually fixed the moon phase for third quarter

--- app/modules/moon_helper.rb
+++ app/modules/moon_helper.rb
@@ -7,7 +7,7 @@ module MoonHelper
       'Plant crops with seeds inside the fruit (beans, peppers, tomatoes, squash and melons).'
     elsif moon_phase.include?('full')
       "Root vegetables, such as carrots, beets, onions and potatoes, will flourish. It's also a good time to transplant seedlings or to prune."
-    elsif moon_phase.include?('waning') || moon_phase.include?('last')
+    elsif moon_phase.include?('waning') || moon_phase.include?('3rd')
       'Avoid planting and focus on fertilizing the soil. This is the best time to mow grass, harvest, transplant and prune.'
     else
       "Interesting, this is a moon we don't have anything for! We will work on that!"
